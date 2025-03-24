eca464c3e78aeab0d9187adb60ad08a344750f42
Riddhi-1928 <riddzzz.28@gmail.com>
2025-03-24 17:51:26 +0530

    small

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://zomato-clone-backend-zeta.vercel.app/'  // Use your deployed backend URL
+    apiUrl: 'https://zomato-clone-backend-zeta.vercel.app/api/auth'  // Use your deployed backend URL
   };
   
